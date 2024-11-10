d5bef62ca75b311e21a929959c02e48916fd4218
clomollo <oyoocliff471@gmail.com>
2024-11-11 00:02:07 +0300

    File structure

---
+++ frontend_react/src/container/Footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.scss'
+
+const footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default footer
---
+++ frontend_react/src/container/Work/Work.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Work'
+
+const Work = () => {
+  return (
+      <div>
+          work
+      
+    </div>
+  )
+}
+
+export default Work
