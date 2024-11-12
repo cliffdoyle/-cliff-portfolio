e35e0329b397ef03439ed4e7f41ddf5acfd6d70c
clomollo <oyoocliff471@gmail.com>
2024-11-12 19:18:03 +0300

    Update social media handles

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsInstagram,BsLinkedin  } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+    <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <BsTwitter />
+        </a>
     </div>
     <div>
-      <FaFacebookF />
+    <a href="https://facebook.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <FaFacebookF />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+    <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+        <BsInstagram />
+        </a>
+    </div>
+    <div>
+    <a href="https://linkedin.com/in/cliff-omollo-14b442269" target="_blank" rel="noopener noreferrer">
+        <BsLinkedin />
+      </a>
     </div>
   </div>
 );
